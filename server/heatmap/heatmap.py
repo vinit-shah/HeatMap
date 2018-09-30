acbd081987bcb7c5f87549c4aa52916a183cbcd9
picklechips <martr4929@hotmail.com>
2018-09-30 17:25:59 -0400

    Try generating heatmap

---
+++ server/heatmap/heatmap.py
@@ -0,0 +1,65 @@
+from collections import namedtuple
+from dataclasses import dataclass
+from PIL import Image
+import numpy as np
+import math
+import StringIO
+
+@dataclass
+class BoundingBox:
+    x: int
+    y: int
+    width: int
+    height: int
+
+class HeatMapGenerator():
+    mFeathering = 40
+    mMaxContrib = 10
+    mContribClip = 300.0
+
+    def __init__(self, width, height, boundingBoxes):
+        self.mWidth = width
+        self.mHeight = height
+        self.mBoundingBoxes = boundingBoxes
+        self.mSumTable = np.zeros(shape=(height, width))
+        self.mHeatMap = np.zeros((height, width, 3), dtype=np.uint8)
+
+    def computeHeatMap(self):
+        for bb in self.mBoundingBoxes:
+            width = bb.width
+            height = bb.height
+            x = bb.x
+            y = bb.y
+
+            cX = int(x + width/2)
+            cY = int(y + height/2)
+            for a in range(cX - self.mFeathering, cX + self.mFeathering):
+                # print(a)
+                if a < 0 or a >= self.mWidth:
+                    continue
+                distA = (cX - a)**2
+                # print(distA)
+                for b in range(cY - self.mFeathering, cY + self.mFeathering):
+                    if b < 0 or b >= self.mHeight :
+                        continue
+                    distB = (cY - b)**2
+                    dist = math.sqrt(distA + distB)
+                    if dist <= self.mFeathering:
+                        self.mSumTable[b,a] += (float(self.mFeathering) - dist)/float(self.mFeathering) * self.mMaxContrib
+
+        for i in range(0, self.mHeight-1):
+            for j in range(0, self.mWidth-1):
+                val = float(self.mSumTable[i,j])/self.mContribClip * 255
+                grayscale = max(0, min(255, int(val)))
+                if grayscale > 255:
+                    print(f'Val {i} {j} {val}')
+                self.mHeatMap[i,j] = [grayscale, grayscale, grayscale]
+
+        self.mImg = Image.fromarray(self.mHeatMap, 'RGB')
+        buf = StringIO.StringIO()
+        self.mImg.save(buf, "PNG")
+        return base64.b64encode(buf.getvalue())
+
+
+
+
--- server/heatmap/views.py
+++ server/heatmap/views.py
@@ -2,7 +2,15 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.staticfiles.templatetags.staticfiles import static
 from .models import Video
-from .forms import VideoForm
+from .forms import VideoForm	
+from collections import deque
+from imutils.video import VideoStream
+import numpy as np
+import argparse
+import cv2
+import imutils
+import time
+import heatmap
 
 # Create your views here.
 def home(request):
@@ -13,8 +21,7 @@ def home(request):
 	if form.is_valid():
 		video = form.save()
 		file = video.file
-		# Gen data from video
-		heatmap = static("leafs.jpeg") # Gen heatmap
+		heatmap = genHeatMap('uploads/' + str(file))
 		video.delete() # Unless we want to save this
 
 	context= {
@@ -24,3 +31,114 @@ def home(request):
               }
     
 	return render(request, 'upload.html', context)
+
+def genHeatMap(file):
+	# list of tracked points
+	greenLower = (108,16,48)
+	greenUpper = (160,255,255)
+	pts = deque(maxlen = 64)
+
+	# grab reference to video supplied
+	vs = cv2.VideoCapture(file)
+
+	# allow the camera or video file to warm up
+	time.sleep(2.0)
+
+	boundingBoxList = []
+	# keep looping
+
+	while True:
+		# grab the current frame
+		frame = vs.read()
+
+		# if we are viewing a video and we did not grab a frame,
+		# then we have reached the end of the video
+		if frame is None:
+			break
+
+		# resize the frame, blur it, and convert it to the HSV
+		# color space
+		frame = imutils.resize(frame, width=600)
+		blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+		hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+
+		# construct a mask for the color "green", then perform
+		# a series of dilations and erosions to remove any small
+		# blobs left in the mask
+		mask = cv2.inRange(hsv, greenLower, greenUpper)
+		mask = cv2.erode(mask, None, iterations=2)
+		mask = cv2.dilate(mask, None, iterations=2)
+
+		# find contours in the mask and initialize the current
+		# (x, y) center of the ball
+		cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+			cv2.CHAIN_APPROX_SIMPLE)
+		cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+		center = None
+
+		# only proceed if at least one contour was found
+		if len(cnts) > 0:
+			# find the largest contour in the mask, then use
+			# it to compute the minimum enclosing circle and
+			# centroid
+			c = max(cnts, key=cv2.contourArea)
+			((x, y), radius) = cv2.minEnclosingCircle(c)
+			M = cv2.moments(c)
+			center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+			topx = int(x - radius)
+			topy = int(y - radius)
+			boundingBox = heatmap.BoundingBox(topx, topy, int(2*radius), int(2*radius))
+			boundingBoxList.append(boundingBox)
+			# only proceed if the radius meets a minimum size
+			if radius > 10:
+				# draw the circle and centroid on the frame,
+				# then update the list of tracked points
+				cv2.circle(frame, (int(x), int(y)), int(radius),
+					(0, 255, 255), 2)
+				cv2.circle(frame, center, 5, (0, 0, 255), -1)
+
+		# update the points queue
+		pts.appendleft(center)
+
+		# loop over the set of tracked points
+		for i in range(1, len(pts)):
+			# if either of the tracked points are None, ignore
+			# them
+			if pts[i - 1] is None or pts[i] is None:
+				continue
+
+			# otherwise, compute the thickness of the line and
+			# draw the connecting lines
+			thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
+			cv2.line(frame, pts[i - 1], pts[i], (0, 0, 255), thickness)
+
+		# show the frame to our screen
+		cv2.imshow("Frame", frame)
+		key = cv2.waitKey(1) & 0xFF
+
+		# if the 'q' key is pressed, stop the loop
+		if key == ord("q"):
+			break
+
+
+	width = int(vs.get(3))
+	height = int(vs.get(4))
+
+	print("width")
+	print(width)
+	print("height")
+	print(height)
+
+	# if we are not using a video file, stop the camera video stream
+	if not args.get("video", False):
+		vs.stop()
+
+	# otherwise, release the camera
+	else:
+		vs.release()
+
+	# close all windows
+	cv2.destroyAllWindows()
+
+	hm = heatmap.HeatMapGenerator(600,396 , boundingBoxList)
+	return hm.computeHeatMap()
