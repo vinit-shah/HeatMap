9a6eddb10d84dade93cad76d0477eb1c00db8d9f
picklechips <martr4929@hotmail.com>
2018-09-30 16:00:35 -0400

    Upload video to server

--- server/heatmap/models.py
+++ server/heatmap/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Video(models.Model):
+    file = models.FileField(upload_to = 'uploads/', null = True, verbose_name = "")
+
+    def __str__(self):
+        return str(self.file)
--- server/heatmap/views.py
+++ server/heatmap/views.py
@@ -1,9 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.contrib.staticfiles.templatetags.staticfiles import static
+from .models import Video
+from .forms import VideoForm
 
 # Create your views here.
 def home(request):
-	image_src = static('leafs.jpeg') # Load in heatmap
-	html = "<html><body><image src='%s'></body></html>" % image_src
-	return HttpResponse(html)
+	file = None
+	heatmap = None
+
+	form = VideoForm(request.POST or None, request.FILES or None)
+	if form.is_valid():
+		video = form.save()
+		file = video.file
+		# Gen data from video
+		heatmap = static("leafs.jpeg") # Gen heatmap
+		video.delete() # Unless we want to save this
+
+	context= {
+	          'file': file,
+              'form': form,
+              'heatmap': heatmap
+              }
+    
+	return render(request, 'upload.html', context)
